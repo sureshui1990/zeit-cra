eb38b5cbd8c88193fe9801ada3d4727624324ab9
Suresh B <sureshui1990@gmail.com>
2020-07-18 13:38:17 +0530

    SemanticUIReact added for UI framework

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+function App() {
+  return (
+        <div>
+        <Container>
+          <h3>Hello World  || Workplace to know about usefull npm package and reactjs features</h3>
+        </Container>
+    </div>
+  );
+}
+
+export default App;
